7fcb50069abf0234999974f05cafe2535c25a3c2
gandythewizard <gandythewizard@gmail.com>
2025-08-24 01:15:13 -0400

    feat: register to MonoLib's Sailing

--- common/src/main/java/com/cursee/disenchanting_table/DisEnchantingTable.java
+++ common/src/main/java/com/cursee/disenchanting_table/DisEnchantingTable.java
@@ -1,11 +1,14 @@
 package com.cursee.disenchanting_table;
 
 import com.cursee.disenchanting_table.platform.Services;
+import com.cursee.monolib.core.sailing.Sailing;
 import net.minecraft.resources.ResourceLocation;
 
 public class DisEnchantingTable {
 
     public static void init() {
+
+        Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
 
         if (Services.PLATFORM.isDevelopmentEnvironment()) {
 
